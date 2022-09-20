1854a6adae1b96b806e0b190aac653880425941c
Louis <louis.haensch01@gmail.com>
2022-09-20 18:26:53 +0200

    Removed unneeded comments

--- web/script.js
+++ web/script.js
@@ -1,30 +1,11 @@
 import init from "/wasm/acquire_rs_wasm.js";
 let wasm = null;
-//let wasm = null;
-//const importObject = {
-//        module: {},
-//        env: {
-//          memory: new WebAssembly.Memory({ initial: 256 }),
-//        }
-//      };
-//WebAssembly.instantiateStreaming(fetch("wasm"), importObject).then(
-//  (results) => {
-//    // Do something with the results!
-//    wasm = results;
-//    console.log("Successfully fetched wasm code!")
-//  }
-//);
-//
-//
+
 async function test() {
     console.log("Hello!");
     console.log(wasm);
     wasm.change_something();
 }
-//
-//function changeButton() {
-//    wasm.instance.exports.change_something();
-//}
 
 document.addEventListener("DOMContentLoaded", async function(){
     console.log("DOM content loaded");
